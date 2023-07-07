a623f48cf7d8f9f6954c1afeb9288f4745507ba2
Пользователь <demidov0129@gmail.com>
2023-07-08 00:48:37 +0300

    update_to_database

--- accountant/main/web_scraping/connect_to_database.py
+++ accountant/main/web_scraping/connect_to_database.py
@@ -6,19 +6,14 @@ def read_from_main_parserconfig():
     try:
         # Connect to the database
         conn = sqlite3.connect('accountant\db.sqlite3')
-        
         # Create a cursor object to execute SQL queries
         cursor = conn.cursor()
-        
         # Execute a SELECT query to retrieve data from a table
         cursor.execute('SELECT * FROM main_parserconfig')
-        
         # Retrieve the results as a list of tuples
         results = cursor.fetchall()
-        
         # Close the database connection
         conn.close()
-        
         # Return the results
         return results
         
@@ -30,19 +25,14 @@ def read_from_main_parserresult():
     try:
         # Connect to the database
         conn = sqlite3.connect('accountant\db.sqlite3')
-        
         # Create a cursor object to execute SQL queries
         cursor = conn.cursor()
-        
         # Execute a SELECT query to retrieve data from a table
         cursor.execute('SELECT * FROM main_parserresult')
-        
         # Retrieve the results as a list of tuples
         results = cursor.fetchall()
-        
         # Close the database connection
         conn.close()
-        
         # Return the results
         return results
         
@@ -54,27 +44,59 @@ def add_to_database(date, time, url):
     try:
         # Connect to the database
         conn = sqlite3.connect('accountant\db.sqlite3')
-        
         # Create a cursor object to execute SQL queries
         cursor = conn.cursor()
-        
         # Get the current maximum ID value from the table
         cursor.execute('SELECT MAX(id) FROM main_parserresult')
         max_id = cursor.fetchone()[0]
         if max_id is None:
             max_id = 0
-        
         # Generate a new ID value
         new_id = max_id + 1
-        
         # Execute an INSERT query to add new data to a table
         cursor.execute('INSERT INTO main_parserresult VALUES (?, ?, ?, ?)', (new_id, date, time, url))
-        
         # Commit the changes to the database
         conn.commit()
-        
         # Close the database connection
         conn.close()
         
+    except sqlite3.Error as error:
+        logger.error(f'Failed to insert data into sqlite table {error}')
+
+
+def update_to_database_parserresult(id, flag):
+    try:
+        # Connect to the SQLite database
+        conn = sqlite3.connect('accountant/db.sqlite3')
+        # Create a cursor object to interact with the database
+        cursor = conn.cursor()
+        # Define the SQL statement to update the flag in the table
+        sql = "UPDATE main_parserresult SET flag = ? WHERE id = ?"
+        # Execute the SQL statement with the flag and id values as parameters
+        cursor.execute(sql, (flag, id))
+        # Commit the changes to the database
+        conn.commit()
+        # Close the cursor and the database connection
+        cursor.close()
+        conn.close()
+    except sqlite3.Error as error:
+        logger.error(f'Failed to insert data into sqlite table {error}')
+
+
+def update_to_database_parserconfig(id, flag):
+    try:
+        # Connect to the SQLite database
+        conn = sqlite3.connect('accountant/db.sqlite3')
+        # Create a cursor object to interact with the database
+        cursor = conn.cursor()
+        # Define the SQL statement to update the flag in the table
+        sql = "UPDATE main_parserconfig SET flag = ? WHERE id = ?"
+        # Execute the SQL statement with the flag and id values as parameters
+        cursor.execute(sql, (flag, id))
+        # Commit the changes to the database
+        conn.commit()
+        # Close the cursor and the database connection
+        cursor.close()
+        conn.close()
     except sqlite3.Error as error:
         logger.error(f'Failed to insert data into sqlite table {error}')
